06a54cb0195f30c336344a08b553afe91275582b
Thanapat Sorralump <thanapat423@gmail.com>
2021-07-25 13:35:58 +0700

    Initial Commit

---
+++ JustAnotherHangMan/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JustAnotherHangMan
+//
+//  Created by Thanapat Sorralump on 25/7/2564 BE.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
